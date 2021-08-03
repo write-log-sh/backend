7378b68684b79ba372df8b976f22194a824e67b7
hyeyoom <neoul_chw@icloud.com>
2021-08-03 21:59:07 +0900

    initial

---
+++ src/main/kotlin/sh/writelog/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package sh.writelog.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendApplication>(*args)
+}
---
+++ src/test/kotlin/sh/writelog/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package sh.writelog.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
