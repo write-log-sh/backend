30ba3c3853a62559415ccb44bcde821b6f0adeff
happy-nut <happynut.dev@gmail.com>
2021-08-23 23:52:33 +0900

    JoinUseCase 추가

---
+++ src/main/kotlin/sh/writelog/backend/user/application/port/inbound/JoinUseCase.kt
@@ -0,0 +1,21 @@
+package sh.writelog.backend.user.application.port.inbound
+
+import kotlin.jvm.Throws
+
+interface JoinUseCase {
+    @Throws(JoinException.EmailAlreadyExists::class)
+    fun execute(command: JoinCommand)
+}
+
+data class JoinCommand(
+    val email: String,
+    val nickname: String,
+    val profileImage: String?,
+    val bio: String?,
+)
+
+sealed class JoinException(message: String) : RuntimeException(message) {
+    object EmailAlreadyExists: JoinException(
+        "이미 가입된 이메일입니다."
+    )
+}
